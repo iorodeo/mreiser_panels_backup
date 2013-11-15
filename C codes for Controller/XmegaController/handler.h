8013a71268c341421f67a78f3d8597a0bdc45b2b
ssafarik <ssafarik@localhost>
2013-11-15 11:13:45 -0800

    Cleaning up global vars & other misc. No functional changes.

--- C codes for Controller/XmegaController/handler.h
+++ C codes for Controller/XmegaController/handler.h
@@ -6,5 +6,5 @@
 
 /* Interrupt Handler Routine Prototypes */
 void Handler_Init(void);
-void Reg_Handler(void* fptr, uint32_t s_cnt, uint8_t priority, uint8_t msk);
-void Update_Reg_Handler(void* fptr, uint32_t s_cnt, uint8_t priority, uint8_t msk);
+void Reg_Handler(void* fptr, uint32_t period, uint8_t index, uint8_t enabled);
+void Update_Reg_Handler(void* fptr, uint32_t period, uint8_t index, uint8_t enabled);
